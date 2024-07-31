a7984fbdb6dc9503749a7ba877fcb0a329cc3c0d
salih.lewagon@gmail.com <salih.lewagon@gmail.com>
2024-07-31 12:51:09 +0000

    finished

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,55 +1,8 @@
-WITH adwords AS (
-    SELECT
-        date_date,
-        paid_source,
-        campaign_key,
-        campaign_name,
-        CAST(ads_cost AS FLOAT64) AS ads_cost,  -- Casting ads_cost to FLOAT64
-        impression,
-        click
-    FROM {{ ref('stg_raw__adwords') }}
-),
-
-bing AS (
-    SELECT
-        date_date,
-        paid_source,
-        campaign_key,
-        campaign_name,
-        CAST(ads_cost AS FLOAT64) AS ads_cost,  -- Casting ads_cost to FLOAT64
-        impression,
-        click
-    FROM {{ ref('stg_raw__bing') }}
-),
-
-criteo AS (
-    SELECT
-        date_date,
-        paid_source,
-        campaign_key,
-        campaign_name,
-        CAST(ads_cost AS FLOAT64) AS ads_cost,  -- Casting ads_cost to FLOAT64
-        impression,
-        click
-    FROM {{ ref('stg_raw__criteo') }}
-),
-
-facebook AS (
-    SELECT
-        date_date,
-        paid_source,
-        campaign_key,
-        campaign_name,
-        CAST(ads_cost AS FLOAT64) AS ads_cost,  -- Casting ads_cost to FLOAT64
-        impression,
-        click
-    FROM {{ ref('stg_raw__facebook') }}
-)
-
-SELECT * FROM adwords
-UNION ALL
-SELECT * FROM bing
-UNION ALL
-SELECT * FROM criteo
-UNION ALL
-SELECT * FROM facebook
+{{ dbt_utils.union_relations(
+    relations=[
+        ref('stg_raw__adwords'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__criteo'),
+        ref('stg_raw__facebook')
+    ]
+) }}

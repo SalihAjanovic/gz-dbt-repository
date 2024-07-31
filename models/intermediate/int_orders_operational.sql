241dc8b972410175267cea815057b16dda5cf646
salih.lewagon@gmail.com <salih.lewagon@gmail.com>
2024-07-31 07:00:19 +0000

    intermediate done

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,47 +1,15 @@
-WITH sales_margin AS (
-    SELECT
-        orders_id,
-        date_date,
-        revenue,
-        margin,
-        purchase_cost
-    FROM {{ ref('int_sales_margin') }}
-),
-
-ship AS (
-    SELECT
-        orders_id,
-        CAST(shipping_fee AS FLOAT64) AS shipping_fee,
-        CAST(log_cost AS FLOAT64) AS log_cost,
-        CAST(ship_cost AS FLOAT64) AS ship_cost
-    FROM {{ ref('stg_raw__ship') }}
-),
-
--- Join sales_margin and ship tables
-joined_data AS (
-    SELECT
-        sm.orders_id,
-        sm.date_date,
-        sm.revenue,
-        sm.margin,
-        sm.purchase_cost,
-        s.shipping_fee,
-        s.log_cost,
-        s.ship_cost,
-        sm.margin + s.shipping_fee - s.log_cost - s.ship_cost AS operational_margin
-    FROM sales_margin sm
-    LEFT JOIN ship s
-    ON sm.orders_id = s.orders_id
-)
-
 SELECT
-    orders_id,
-    date_date,
-    revenue,
-    margin,
-    purchase_cost,
-    shipping_fee,
-    log_cost,
-    ship_cost,
-    operational_margin
-FROM joined_data
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.log_cost + s.ship_cost),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
+  ,s.shipping_fee
+  ,s.log_cost
+  ,s.ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw__ship")}} s 
+  USING(orders_id)
+ORDER BY orders_id desc

e713dba0623d35eeb198b7253587dfa705161d57
salih.lewagon@gmail.com <salih.lewagon@gmail.com>
2024-07-31 06:53:00 +0000

    lol

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,42 +1,12 @@
-WITH sales AS (
-    SELECT
-        orders_id,
-        products_id,
-        date_date, -- Ensure this column is included
-        revenue,
-        CAST(quantity AS FLOAT64) AS quantity
-    FROM {{ ref('stg_raw__sales') }}
-),
-
-product AS (
-    SELECT
-        products_id,
-        CAST(purchase_price AS FLOAT64) AS purchase_price
-    FROM {{ ref('stg_raw__product') }}
-),
-
--- Join sales and product tables
-sales_with_cost AS (
-    SELECT
-        s.orders_id,
-        s.products_id,
-        s.date_date, -- Ensure this column is included
-        s.revenue,
-        s.quantity,
-        p.purchase_price,
-        s.quantity * p.purchase_price AS purchase_cost
-    FROM sales s
-    INNER JOIN product p
-    ON s.products_id = p.products_id
-)
-
-SELECT
+SELECT 
+    products_id, 
+    date_date, 
     orders_id,
-    products_id,
-    date_date, -- Ensure this column is included
-    quantity,
-    revenue,
-    purchase_price,
-    purchase_cost,
-    revenue - purchase_cost AS margin
-FROM sales_with_cost
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64), 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+    USING (products_id)
